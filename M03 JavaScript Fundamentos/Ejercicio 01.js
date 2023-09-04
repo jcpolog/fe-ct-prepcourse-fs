71dd2289d3063e4d9be5fdd71f080df6ec940196
unknown <polo1972gonzalez@gmail.com>
2023-09-04 11:58:12 -0500

    Mi primer ejercicio

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,22 +5,37 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = 'Polo';
+// creo una variable llamada tipo y de tipo string por que la pongo entre las comillas
+//var tipo = 'String';
+
 
 // Crea una variable de tipo number.
-const nuevoNumero = null;
+const nuevoNumero = 5;
+//var x = 5;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = null;
+const nuevoBoolean = true;
+// si es falso o verdadero
+//var meguastProgramar = true;
+
+var megustaProgramar = false;
 
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - null === 3;
+const nuevaResta = 10 - 7 === 3;
+//La respuesta es 7
+//nuevaResta (7);
+
 
 // Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * null === 40;
+const nuevaMultiplicacion = 10 * 4 === 40;
+//La respuesta es 4
+
 
 // Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
+// la respuesta es 1.
+//nuevoModulo (1);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
