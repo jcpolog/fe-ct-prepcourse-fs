a81c44fe9e513317635db844fa7abc04a177971a
unknown <polo1972gonzalez@gmail.com>
2023-09-04 12:32:48 -0500

    Ejecicio 04 subido

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,40 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   //match.pow(num, 2);
+   return Math.pow(num, 2); 
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   return Math.pow(num, 3);
 }
 
 function elevar(num, exponent) {
-   // Retorna el valor de "num" elevado al exponente "exponent".
-   // Tu código:
+   // Retorna el valor de "num" elevado al exponente "exponent".   // Tu código:
+   return Math.pow(num, exponent,3);
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   //return Math.round(0.49);
+   return Math.round(num, 0.49);
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   
+//return matc.ceil(num, 3.27);
+return Math.ceil(num, 0.49);
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.random();
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
